2641dc8ab6b3b4dd6df965ed8ea3f4a34071ba4f
PedroMoreiraDev <pedrodev2020@gmail.com>
2022-02-20 00:52:21 -0300

    enviando testes unitários cliente(por id)

---
+++ logistica-api-finalizado/logistica-api/src/main/resources/sql/insert_nutri_para_teste_buscar_por_id.sql
@@ -0,0 +1,2 @@
+delete from cliente;
+insert into cliente (id, email, nome, fone) values (55, 'pedro123@gmail.com', 'pedro', '11999999')
--- logistica-api-finalizado/logistica-api/src/test/java/com/example/logisticaapi/LogisticaApiApplicationTests.java
+++ logistica-api-finalizado/logistica-api/src/test/java/com/example/logisticaapi/LogisticaApiApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.logisticaapi;
 
+import com.example.logisticaapi.domain.model.Cliente;
+import com.example.logisticaapi.domain.model.Entrega;
+import com.example.logisticaapi.domain.repository.ClienteRepository;
+import com.example.logisticaapi.domain.service.BuscaEntregaService;
+import com.example.logisticaapi.domain.service.CatalogoClienteService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@Sql(scripts = "/sql/insert_nutri_para_teste_buscar_por_id.sql")
 class LogisticaApiApplicationTests {
 
+	@Autowired
+	private CatalogoClienteService catalogoClienteService;
+
+	@Autowired
+	ClienteRepository clienteRepository;
+
 	@Test
-	void contextLoads() {
+	void buscarClienteTest() {
+		Cliente cliente = catalogoClienteService.buscar(55L);
+
+		assertEquals("pedro", cliente.getNome());
+		assertEquals("pedro123@gmail.com", cliente.getEmail());
+	}
+
+	@Test
+	public void deletaPorIdTest(){
+		catalogoClienteService.excluir(55L);
+
+		Optional<Cliente> optionalCliente = clienteRepository.findById(55L);
+
+		assertFalse(optionalCliente.isPresent());
 	}
 
 }
